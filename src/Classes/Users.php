2d6fe23d611fe8da195a901609e4de6ffaab3d3c
Alexander <alexander.zugan@yandex.ru>
2018-07-23 15:56:26 +0300

    add users methods and some fix

--- src/Classes/Users.php
+++ src/Classes/Users.php
@@ -58,9 +58,95 @@ class Users extends Request
         return $this->get("users/{$userId}", $optional);
     }
 
+    /**
+     * Remove
+     *
+     * @param string $userId
+     * @return array|mixed
+     */
     public function remove(string $userId)
     {
         return $this->delete("users/{$userId}");
+    }
+
+    /**
+     * Users Assistants List
+     *
+     * @param string $userId
+     * @return array|mixed
+     */
+    public function assistantsList(string $userId)
+    {
+        return $this->get("users/{$userId}/assistants");
+    }
+
+    /**
+     * Add Assistant
+     *
+     * @param string $userId
+     * @param array $data
+     * @return array|mixed
+     */
+    public function addAssistant(string $userId, array $data)
+    {
+        return $this->post("users{$userId}/assistants", $data);
+    }
+
+    /**
+     * Delete Assistants
+     *
+     * @param string $userId
+     * @return array|mixed
+     */
+    public function deleteAssistants(string $userId)
+    {
+        return $this->delete("users/{$userId}/assistants");
+    }
+
+    /**
+     * Delete Assistant
+     *
+     * @param string $userId
+     * @param string $assistantId
+     * @return array|mixed
+     */
+    public function deleteAssistant(string $userId, string $assistantId)
+    {
+        return $this->delete("users/{$userId}/assistants/{$assistantId}");
+    }
+
+    /**
+     * Schedulers List
+     *
+     * @param string $userId
+     * @return array|mixed
+     */
+    public function schedulersList(string $userId)
+    {
+        return $this->get("users/{$userId}/schedulers");
+    }
+
+    /**
+     * Deletes Schedulers
+     *
+     * @param string $userId
+     * @return array|mixed
+     */
+    public function deletesSchedulers(string $userId)
+    {
+        return $this->delete("users/{$userId}/schedulers");
+    }
+
+    /**
+     * Deletes Scheduler
+     *
+     * @param string $userId
+     * @param string $schedulerId
+     * @return array|mixed
+     */
+    public function deletesScheduler(string $userId, string $schedulerId)
+    {
+        return $this->delete("users/{$userId}/schedulers/{$schedulerId}");
     }
 
 }
--- src/config/zoom.php
+++ src/config/zoom.php
@@ -2,7 +2,6 @@
 
 return [
 
-    'api_key' => env('ZOOM_KEY'),
-    'api_secret' => env('ZOOM_SECRET'),
-    'user_id' => env('ZOOM_USER_ID'),
+    'api_key' => '',
+    'api_secret' => '',
 ];
